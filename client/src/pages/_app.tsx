65bc9afbf35b24d0d05f65a504f83c94b7012be5
devinMcArthur <assortedgerm@gmail.com>
2022-03-18 13:47:51 -0600

    Add additional data to Head for PWA

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -11,6 +11,30 @@ export default function App({ Component, pageProps }: AppProps) {
     <Provider>
       <Head>
         <title>Bow Mark</title>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+        />
+        <meta name="description" content="Bow Mark Web Application" />
+        <meta name="keywords" content="Keywords" />
+
+        <link rel="manifest" href="/manifest.json" />
+        <link
+          href="/icons/favicon-16x16.png"
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+        />
+        <link
+          href="/icons/favicon-32x32.png"
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+        />
+        <link rel="apple-touch-icon" href="/apple-icon.png"></link>
+        <meta name="theme-color" content="#ef2e25" />
       </Head>
       <Navbar />
       <MainPageContainer>
