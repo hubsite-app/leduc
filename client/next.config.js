a7ece48ebfc2791c1c66cb81470cdc7d5ad01bdb
devinMcArthur <assortedgerm@gmail.com>
2022-03-18 18:49:16 -0600

    Fix pwa setup

--- client/next.config.js
+++ client/next.config.js
@@ -6,9 +6,11 @@ const withPWA = require("next-pwa");
 const runtimeCaching = require("next-pwa/cache");
 
 const nextConfig = withPWA({
-  dest: "public",
-  runtimeCaching,
-  disable: process.env.NODE_ENV === "development",
+  pwa: {
+    dest: "public",
+    runtimeCaching,
+    // disable: process.env.NODE_ENV === "development",
+  },
 });
 
 module.exports = nextConfig;
