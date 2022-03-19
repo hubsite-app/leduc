934ffa8d0da16d93ca84ff106b145ab4cceee4f6
devinMcArthur <assortedgerm@gmail.com>
2022-03-18 18:25:15 -0600

    Test PWA

--- client/next.config.js
+++ client/next.config.js
@@ -1,18 +1,14 @@
 /**
  * @type {import('next').NextConfig}
  */
-const nextConfig = {
-  webpackDevMiddleware: (config) => {
-    config.watchOptions = {
-      poll: 800,
-      aggregateTimeout: 300,
-    };
-    return config;
-  },
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
-  },
-};
+
+const withPWA = require("next-pwa");
+const runtimeCaching = require("next-pwa/cache");
+
+const nextConfig = withPWA({
+  dest: "public",
+  runtimeCaching,
+  disable: process.env.NODE_ENV === "development",
+});
 
 module.exports = nextConfig;
---
+++ client/src/pages/_documents.tsx
@@ -0,0 +1,36 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <meta charSet="utf-8" />
+          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+          <meta name="description" content="Bow Mark Web Application" />
+          <meta name="keywords" content="Keywords" />
+
+          <link rel="manifest" href="/manifest.json" />
+          <link
+            href="/icons/favicon-16x16.png"
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+          />
+          <link
+            href="/icons/favicon-32x32.png"
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+          />
+          <link rel="apple-touch-icon" href="/apple-icon.png"></link>
+          <meta name="theme-color" content="#ef2e25" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
