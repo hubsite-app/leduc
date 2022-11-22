ddfde38955e55dd255414d9ece7ce841e15c4f63
devinMcArthur <assortedgerm@gmail.com>
2022-11-21 19:03:36 -0700

    Cleanup playground

--- client/src/components/pages/playground/index.tsx
+++ client/src/components/pages/playground/index.tsx
@@ -1,34 +1,15 @@
 import React from "react";
-import { useJobsiteFullQuery } from "../../../generated/graphql";
-import Panel from "../../Common/Panel";
-import JobsiteMaterialsCosting from "../jobsite/id/views/JobsiteMaterials";
 
 const PlaygroundClientOnly = () => {
   /**
    * ----- Hook Initialization -----
    */
 
-  const { data, loading } = useJobsiteFullQuery({
-    variables: {
-      id: "6266c5af27beaf001215b57b",
-    },
-  });
-
   /**
    * ----- Render -----
    */
 
-  const content = React.useMemo(() => {
-    if (data?.jobsite) {
-      return <JobsiteMaterialsCosting jobsite={data.jobsite} />;
-    } else return null;
-  }, [data?.jobsite]);
-
-  return (
-    <Panel name="Jobsite Materials" loading={loading}>
-      {content}
-    </Panel>
-  );
+  return <div>client stuff</div>;
 };
 
 export default PlaygroundClientOnly;
