0d9f290ef1a9b4fb72ecef01e42a659eb5bfac17
devinMcArthur <assortedgerm@gmail.com>
2023-03-08 11:05:13 -0700

    Update next-config

--- client/next.config.js
+++ client/next.config.js
@@ -2,15 +2,15 @@
  * @type {import('next').NextConfig}
  */
 
-const withPWA = require("next-pwa");
 const runtimeCaching = require("next-pwa/cache");
 
-const nextConfig = withPWA({
-  pwa: {
-    dest: "public",
-    runtimeCaching,
-    disable: process.env.NODE_ENV === "development",
-  },
+const withPWA = require("next-pwa")({
+  dest: "public",
+  runtimeCaching,
+  disable: process.env.NODE_ENV === "development",
 });
 
-module.exports = nextConfig;
+module.exports = withPWA({
+  // next.js config
+  // reactStrictMode: true,
+});
