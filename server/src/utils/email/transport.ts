54be8666b683b1ee6d19b79194f26362e98c60b6
devinMcArthur <assortedgerm@gmail.com>
2023-04-03 16:07:00 -0600

    Trigger build

--- server/src/utils/email/transport.ts
+++ server/src/utils/email/transport.ts
@@ -1,7 +1,7 @@
 import nodemailer from "nodemailer";
 
 export default function transport() {
-  console.log({
+  console.log("ENV", {
     host: process.env.EMAIL_HOST as string,
     port: parseInt(process.env.EMAIL_PORT as string),
     secure: true,
