68ad26d9fc3565fef4d1b8d6b6bd2281d53b7f98
devinMcArthur <assortedgerm@gmail.com>
2023-04-03 16:00:59 -0600

    debug email service

--- server/src/utils/email/transport.ts
+++ server/src/utils/email/transport.ts
@@ -1,6 +1,15 @@
 import nodemailer from "nodemailer";
 
 export default function transport() {
+  console.log({
+    host: process.env.EMAIL_HOST as string,
+    port: parseInt(process.env.EMAIL_PORT as string),
+    secure: true,
+    auth: {
+      user: process.env.EMAIL_USERNAME as string,
+      pass: process.env.EMAIL_PASSWORD as string,
+    },
+  });
   return nodemailer.createTransport({
     host: process.env.EMAIL_HOST as string,
     port: parseInt(process.env.EMAIL_PORT as string),
